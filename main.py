fdc0cc9aee292cca10b94ac841c28113e15d9790
roe <kulakov@03.perm.ru>
2017-08-23 09:52:42 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,39 @@
+import requests
+import json
+
+url = "http://10.1.11.102:8080/"
+headers = {'content-type': 'application/json'}
+
+def server_info():
+    # Example echo method
+    payload = {
+        "method": "server_info",
+        "params": [
+        {}
+        ]
+    }
+    response = requests.post(url, data=json.dumps(payload), headers=headers)
+    print response.json()
+    #return response
+def account_info():
+    payload = {
+        "method": "account_info",
+        "params": [
+            {
+            "account": "rG1QQv2nh2gr7RCZ1P8YYcBUKCCN633jCn",
+            "strict": "true",
+            "ledger_index": "current",
+            "queue": "true"
+            }
+        ]
+    }
+    response = requests.post(url, data=json.dumps(payload), headers=headers)
+    print response.json()
+    
+def main():
+    server_info()
+    print "=========================="
+    account_info()
+
+if __name__ == "__main__":
+    main()
