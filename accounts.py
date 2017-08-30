2baa7d0e0d92d3d1f90912c65a24372abcb17361
roe <kulakov@03.perm.ru>
2017-08-30 13:35:45 +0500

    add accounts

---
+++ accounts.py
@@ -0,0 +1,10 @@
+company = {'issuing': {'address': 'rPCy6pvtxk81QRJqULa3UyWhNXKtnmPGCr', 'secret': 'snguvGC8vbzyVAaRM6Dzfvxynp6mD'},
+           'operational': {'address': 'rs3hz8otUSxzWRRJfDHC5VZ13zigtuJkvM', 'secret': 'snJ5ZsWhuBex8oWDZjmUZUa7fSpa5'}
+           }
+
+
+accounts = [{'address': 'r3AJ6YZucYfd7pcvLh22b19PB5qrNc6HgT', 'secret': 'ssg1ATcR8rN67hg3zpbP3NSLMRXru'},
+            {'address': 'rapEGXN8hhg1PtB9zm6uGTBdxhFRHti9JB', 'secret': 'snFwteTtz5ntTePHiEjAWSeea7TiV'},
+            {'address': 'rGTgBytUd5vqJ44HgMH5Sk7BPduPYnS5ZL', 'secret': 'spoHntTVAjrUcQcsHJsEGmSqD68Sk'},
+            {'address': 'rHDdV2e7Y81Y7fX1E8hPGG4oG7t2u9sGHP' , 'secret': 'shxzWn4aUWwmGFMRGyWkjHD6ANjHN'},
+            {'address': 'rBoyk5Fh9ozNr3cizS2M7P4ySnJkbcCyEC' , 'secret': 'sn37UwfLRNJ7AxrkYmKYWe5zRnL1u'}]
